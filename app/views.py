fd0dd5b6acd15ca98672d04ff15f7786e262d164
yvonnosa <osawayvonne@gmail.com>
2017-10-22 13:46:51 -0700

    [Chore]intergreted my templates to run n app

---
+++ app/views.py
@@ -0,0 +1,38 @@
+# views.py
+
+from flask import render_template
+
+from app import app
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+    @app.route('/login')
+def login():
+    return render_template("login.html")
+
+    @app.route('/create')
+def create():
+    return render_template("create.html")
+
+    @app.route('/view')
+def view():
+    return render_template("view.html")
+
+    @app.route('/edit')
+def edit():
+    return render_template("edit.html")
+
+
+
+
+
+
+
+
+    
